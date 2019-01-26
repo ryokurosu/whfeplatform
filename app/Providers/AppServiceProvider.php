615776af26c98d7f51cb239c4391abbd6c4e0aa6
Ryo Kurosu <knowrop1208@gmail.com>
2019-01-27 00:47:45 +0900

    wa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if(config('app.env') === 'production'){
+    // asset()やurl()がhttpsで生成される
+            URL::forceScheme('https');
+        }
     }
 
     /**
