f4afa07c3aca664372638b6cca47d3a1bfbbfd6a
Ryo Kurosu <knowrop1208@gmail.com>
2019-01-27 01:02:04 +0900

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(config('app.env') === 'production'){
-    // asset()やurl()がhttpsで生成される
-            URL::forceScheme('https');
+       if ($this->app->environment() == 'production') {
+            \URL::forceScheme('https');
         }
     }
 
