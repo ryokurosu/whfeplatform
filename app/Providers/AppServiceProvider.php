777dbd999148184e0f94327594fc0ea5d374710b
Ryo Kurosu <knowrop1208@gmail.com>
2019-01-27 03:45:45 +0900

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-       if ($this->app->environment() == 'production') {
-            \URL::forceScheme('https');
-        }
+       // if ($this->app->environment() == 'production') {
+       //      \URL::forceScheme('https');
+       //  }
     }
 
     /**
